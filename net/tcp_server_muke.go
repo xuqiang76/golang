d08ad5529312c55722489e3b916c6467f0108ba5
xuqiang <xuqiang76@163.com>
2017-11-28 10:29:39 +0800

    net/tcp_server_muke.go

--- net/tcp_server_muke.go
+++ net/tcp_server_muke.go
@@ -1,42 +1,42 @@
-package main 
+package main
 
 import (
-    "net"
-    "log"
+	"log"
+	"net"
 )
 
-func func main() {
-    listen, err := net.Listen("tcp", "127.0.0.1:1234")
-    if err != nil {
-        return
-    }
+func main() {
+	listen, err := net.Listen("tcp", "127.0.0.1:1234")
+	if err != nil {
+		return
+	}
 
-    for {
-        conn, err := listen.Accept()
-        if err != nil {
-            continue
-        }
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			continue
+		}
 
-        go handle_conn(conn)
-    }
+		go handle_conn(conn)
+	}
 
-    defer func () {
-        listen.Close()
-    }()
+	defer func() {
+		listen.Close()
+	}()
 }
 
 func handle_conn(conn net.Conn) {
-    buf := make([]byte, 1024)
+	buf := make([]byte, 1024)
 
-    defer conn.Close()
+	defer conn.Close()
 
-    for {
-        len, err := conn.Read(buf)
-        if err != nil {
-            return
-        }
+	for {
+		len, err := conn.Read(buf)
+		if err != nil {
+			return
+		}
 
-        log.Print("recv msg ", string(buf[0:len]))
-        conn.Write([]byte("server write "))
-    }
+		log.Print("recv msg ", string(buf[0:len]))
+		conn.Write([]byte("server write "))
+	}
 }
