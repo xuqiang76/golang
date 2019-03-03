b1b2b016a2c81fc4334269fa73c12a3470e80471
xuqiang <xuqiang76@163.com>
2019-03-03 12:58:54 +0800

    add

--- tuling/code/p1/gcd.go
+++ tuling/code/p1/gcd.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 //辗转相除法求最大公约数
-func gcd(x, y int) int {
-	var tmp int
+func gcd(x, y uint) uint {
+	var tmp uint
 	tmp = x % y
 	if tmp > 0 {
 		return gcd(y, tmp)
@@ -15,7 +15,8 @@ func gcd(x, y int) int {
 }
 
 func main() {
-	var x, y int
+	var x, y uint
 	x, y = 12, 15
 	fmt.Println("gcd =", gcd(x, y))
+	fmt.Println("gcd =", gcd(100, 120))
 }
